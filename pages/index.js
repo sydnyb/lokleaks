bc7f9554ad70b407e967d9ce3674ead2b8b5160a
Syed Nayab <nayab99@gmail.com>
2025-06-30 16:50:10 +0530

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'Arial' }}>
+      <h1>Welcome to LokLeaks</h1>
+      <p>This is the starting page for the anti-corruption complaint system.</p>
+    </div>
+  );
+}
